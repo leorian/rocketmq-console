c1815c6c6f6a96ece2f020d813605735e0ca967f
xiezg <xiezg@317hu.com>
2017-06-02 08:20:53 +0800

    rocketmq管理平台

---
+++ src/main/java/Main.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        while (true) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println(Thread.currentThread().getName() + new Date().getTime());
+                    try {
+                        System.in.read();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+        }
+    }
+}
