27ee84934cca637c9c3db04bfcc3b81859b181b2
xiezg <xiezg@317hu.com>
2017-06-15 11:22:07 +0800

    http==https

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,19 +2,19 @@ import java.io.IOException;
 import java.util.Date;
 
 public class Main {
-    public static void main(String[] args) {
-        while (true) {
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    System.out.println(Thread.currentThread().getName() + new Date().getTime());
-                    try {
-                        System.in.read();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }).start();
-        }
-    }
+//    public static void main(String[] args) {
+//        while (true) {
+//            new Thread(new Runnable() {
+//                @Override
+//                public void run() {
+//                    System.out.println(Thread.currentThread().getName() + new Date().getTime());
+//                    try {
+//                        System.in.read();
+//                    } catch (IOException e) {
+//                        e.printStackTrace();
+//                    }
+//                }
+//            }).start();
+//        }
+//    }
 }
